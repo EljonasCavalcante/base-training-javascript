b9eac661c8c119bd25470b3ff4b203c43522dbf6
EljonasCavalcante <eljonascavalcante@gmail.com>
2022-07-31 19:44:31 -0300

    feat: create update

--- DOM/Organizado/script_preventDefault.js
+++ DOM/Organizado/script_preventDefault.js
@@ -1,16 +1,10 @@
-const animaisList = document.querySelector(".animais-lista");
 
-function callbackList(event) {
-  console.log(event.target);
-}
-animaisList.addEventListener("click", callbackList);
-
-//============= preventDefault======================
 const linkExterno = document.querySelector('a[href^="http"]');
 
 function handleLinkExterno(event) {
-  event.preventDefault(); // server para previr o padrão ou desativar o padrão.
-  console.log("clicou");
+    event.preventDefault(); // previnindo que aconteça o padrão.
+    console.log(event, this)// this se refere a primeiro elemento no caso o link externo
+    console.log(event.currentTarget)// faz a mesma coisa que o THIS
 }
 
-linkExterno.addEventListener("click", handleLinkExterno);
+linkExterno.addEventListener('click', handleLinkExterno)
