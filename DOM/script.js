2af602f2a481aa87184fc5d41cc3131a90bac078
EljonasCavalcante <eljonascavalcante@gmail.com>
2022-08-05 00:05:00 -0300

    accordion-list

--- DOM/script.js
+++ DOM/script.js
@@ -1,24 +1,30 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-console.log(tabMenu)
-const tabContent = document.querySelectorAll('.js-tabcontent section');
-console.log(tabContent)
+function initTabNav() {
+     const tabMenu = document.querySelectorAll('.js-tabmenu li');
+    console.log(tabMenu)
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
+    console.log(tabContent)
 
-if(tabMenu.length && tabContent.length) {
+     //faz texto sempre ficar ativo no iniciar.
+    if(tabMenu.length && tabContent.length) {       
+        tabContent[0].classList.add('ativo')
 
-    //faz texto sempre ficar ativo no iniciar.
-tabContent[0].classList.add('ativo')
+        function activeTab(index) {
+         tabContent.forEach((section)=>{  // fazendo um loop para remover a classe desejada  
+             section.classList.remove('ativo');// removendo classe
+         })
+         tabContent[index].classList.add('ativo');// adcionando classe
+        }
 
-function activeTab(index) {
-    tabContent.forEach((section)=>{  // fazendo um loop para remover a classe desejada  
-        section.classList.remove('ativo');// removendo classe
+
+        tabMenu.forEach((itemMenu,index) => { // fazendo um loop argumento, itemMenu e o index que seria a array
+            itemMenu.addEventListener('click', () => { // em cada item eu adicono um evento de click
+                activeTab(index)// quando o click for clicado ele ativa a função que recebe o index
+            })
     })
-    tabContent[index].classList.add('ativo');// adcionando classe
+    }
 }
+initTabNav();
 
-
-tabMenu.forEach((itemMenu,index) => { // fazendo um loop argumento, itemMenu e o index que seria a array
-    itemMenu.addEventListener('click', () => { // em cada item eu adicono um evento de click
-        activeTab(index)// quando o click for clicado ele ativa a função que recebe o index
-    })
-})
-}
+/*============= Accordion-list=================*/
+const faq = document.querySelectorAll('.faq dt');
+console.log(faq)
