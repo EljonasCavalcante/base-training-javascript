14ed3e89470fd16a28a391bea6a697e363bfda6d
EljonasCavalcante <eljonascavalcante@gmail.com>
2022-08-30 21:50:05 -0300

    add every function

---
+++ Obj/someEvery.js
@@ -0,0 +1,16 @@
+/* Some() , se pelo menos um return da iteração For truthy. ele retorna true */
+
+const frutas = ['Batana', 'Pêra','Uva'];
+
+const temUva = frutas.some((item) => {
+    console.log(item) //Batana  Pêra   Uva 
+    return item === 'Uva';
+})
+
+
+const every = frutas.every((item) => {
+    console.log(item) //Batana  Pêra   Uva 
+    return item === 'Uva';
+})
+
+console.log(temUva)// true
