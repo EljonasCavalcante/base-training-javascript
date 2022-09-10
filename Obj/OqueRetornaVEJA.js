30e3c3523c95ff426967556ab9cef8f714d8c11b
EljonasCavalcante <eljonascavalcante@gmail.com>
2022-09-10 12:46:14 -0300

    prototype element

---
+++ Obj/OqueRetornaVEJA.js
@@ -0,0 +1,17 @@
+// entenda o que retorna para saber o que fazer.
+
+const Carro = {
+    marca: 'Ford',
+    preco: 2000,
+    acelerar() {
+        return true
+    }
+}
+
+Carro // Object
+Carro.marca // String
+Carro.preco // Number
+Carro.acelerar // Function
+Carro.acelerar() // Boolean
+Carro.marca.charAt // Function
+Carro.marca.charAt(0) // String
