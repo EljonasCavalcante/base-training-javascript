945af2ef8277187f1e957fba41f01bd76d162c6f
EljonasCavalcante <eljonascavalcante@gmail.com>
2022-09-10 15:48:40 -0300

    simple

---
+++ Obj/exercioNative.js
@@ -0,0 +1,15 @@
+// Liste 5 objetos nativos
+Object
+Number
+Array
+Function
+String
+// Liste 5 objetos do browser
+Window
+Document
+HTMLAllCollection
+NodeList
+history
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
