2614b200b7b5c13622a79ae7291c67de3f6c655f
EljonasCavalcante <eljonascavalcante@gmail.com>
2022-09-13 21:50:02 -0300

    create file

---
+++ Obj/script01.js
@@ -0,0 +1,13 @@
+
+// Crie uma função para limpar os preços
+// e retornar os números com centavos arredondados
+// depois retorne a soma total
+const listaPrecos = ['R$ 59,99', ' R$ 100,222',
+                     'R$ 230  ', 'r$  200'];
+
+
+function limparPreco(preco) {
+    preco = preco.toUpperCase()
+}
+console.log(listaPrecos[1])
+
