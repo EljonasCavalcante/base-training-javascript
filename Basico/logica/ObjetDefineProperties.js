4622ba211b64ed6c28c6814cb701eba9c45c5eb9
EljonasCavalcante <eljonascavalcante@gmail.com>
2023-01-05 21:50:43 -0300

    feat: create file object define properties

---
+++ Basico/logica/ObjetDefineProperties.js
@@ -0,0 +1,20 @@
+// Object.defineProperties(alvo, propriedades) adiciona ao alvo novas propriedades. A diferença aqui é a possibilidade de serem definidas as características dessas propriedades.
+
+const moto = {}
+Object.defineProperties(moto, {
+  rodas: {
+    value: 2,
+    configurable: false, // impede deletar e mudança de valor
+    enumarable: true, // torna enumerável
+  },
+  capacete: {
+    value: true,
+    configurable: true,
+    writable: false, // impede mudança de valor
+  },
+})
+
+moto.rodas = 4;
+delete moto.capacete;
+moto;
+// {rodas: 2}
