810273f861d046d1259cdbec64749dae7f5223fb
EljonasCavalcante <eljonascavalcante@gmail.com>
2023-01-05 22:13:18 -0300

    feat: create file Obejct Get property Descriptors two

---
+++ Basico/logica/ObjectGetownPropertyDescriptors.js
@@ -0,0 +1,8 @@
+Object.getOwnPropertyDescriptors(Array);
+// Lista com métodos e propriedades e Array
+
+Object.getOwnPropertyDescriptors(Array.prototype);
+// Lista com métodos e propriedades do protótipo de Array
+
+Object.getOwnPropertyDescriptor(window, 'innerHeight');
+// Puxa de uma única propriedade - procura um item especifico
