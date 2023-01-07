b9eed9f349a7d0f1e4215c4c2f232ecf46eb2759
EljonasCavalcante <eljonascavalcante@gmail.com>
2023-01-06 23:26:38 -0300

    feat: create dataset

---
+++ Basico/Efeito do DOM/dataset.js
@@ -0,0 +1,3 @@
+
+// dataset é uma propriedade de HTMLElement, essa propriedade é um objeto do tipo DOMStringMap. Dentro desse objeto existe uma coleção de chave / valor, com todos os atributos do elemento html que começarem com data-.
+
