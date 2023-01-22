109f9149a354edc2280478dc04d6760b17cd5564
EljonasCavalcante <eljonascavalcante@gmail.com>
2023-01-22 00:23:33 -0300

    objetc

---
+++ Basico/logica/objetoAssign.js
@@ -0,0 +1,22 @@
+const funcaoAutomovel = {
+    acelerar() {
+      return 'acelerou';
+    },
+    buzinar() {
+      return 'buzinou';
+    },
+  }
+  
+  const moto = {
+    rodas: 2,
+    capacete: true,
+  }
+  
+  const carro = {
+    rodas: 4,
+    mala: true,
+  }
+  
+  Object.assign(moto, funcaoAutomovel);
+  Object.assign(carro, funcaoAutomovel);
+  
