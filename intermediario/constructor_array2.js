56ebe00f50819538527e59bf4f6e89573ec015cb
EljonasCavalcante <eljonascavalcante@gmail.com>
2023-01-31 23:03:02 -0300

    array

---
+++ intermediario/constructor_array2.js
@@ -0,0 +1,9 @@
+const frutas = ['Banana', 'Pêra', ['Uva Roxa', 'Uva Verde']];
+
+console.log(frutas.length); // 3 ['banana', 'pêra' , {...}]
+
+frutas[0].length; // 6
+frutas[1].length; // 5
+frutas[2].length; // 2
+console.log(frutas[2][0].length); // 8 ['Uva Roxa']
+
