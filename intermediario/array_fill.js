25090372700653a1d25dd27b4fdce6505c727742
EljonasCavalcante <eljonascavalcante@gmail.com>
2023-01-31 23:19:22 -0300

    fill

---
+++ intermediario/array_fill.js
@@ -0,0 +1,4 @@
+//[].fill(valor, inicio, final) preenche a array com o valor, do início até o fim.
+const preenche = ['Item1', 'Item2', 'Item3', 'Item4'].fill('Banana');
+//[ 'Banana', 'Banana', 'Banana', 'Banana' ]
+console.log(preenche)
